3fd40075cdb74edf43939ebb9244237a211ad49d
Nallapatiakhila <akhilanallapati3@gmail.com>
2026-03-15 01:34:56 +0530

    Update CorsConfig.java

--- src/main/java/com/example/backend/config/CorsConfig.java
+++ src/main/java/com/example/backend/config/CorsConfig.java
@@ -17,7 +17,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://tt-frontend-odoz.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
