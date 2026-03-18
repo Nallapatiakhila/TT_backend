30f2f97172a799b1ecc324c9422b1f8fbf5f5680
Nallapatiakhila <akhilanallapati3@gmail.com>
2026-03-18 15:02:52 +0530

    Update OtpService.java

--- src/main/java/com/example/backend/service/OtpService.java
+++ src/main/java/com/example/backend/service/OtpService.java
@@ -1,3 +1,50 @@
+// package com.example.backend.service;
+
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.mail.SimpleMailMessage;
+// import org.springframework.mail.javamail.JavaMailSender;
+// import org.springframework.stereotype.Service;
+
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.Random;
+
+// @Service
+// public class OtpService {
+
+//     @Autowired
+//     private JavaMailSender mailSender;
+
+//     // store OTP temporarily
+//     private Map<String, String> otpStorage = new HashMap<>();
+
+//     public void sendOtp(String email) {
+
+//         String otp = String.valueOf(new Random().nextInt(900000) + 100000);
+
+//         otpStorage.put(email, otp);
+
+//         SimpleMailMessage message = new SimpleMailMessage();
+//         message.setTo(email);
+//         message.setSubject("SmartPlan AI - OTP Verification");
+//         message.setText("Your OTP is: " + otp);
+
+//         mailSender.send(message);
+//     }
+
+//     public boolean verifyOtp(String email, String otp) {
+
+//         String storedOtp = otpStorage.get(email);
+
+//         if (storedOtp != null && storedOtp.equals(otp)) {
+//             otpStorage.remove(email);
+//             return true;
+//         }
+
+//         return false;
+//     }
+// }
+
 package com.example.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
